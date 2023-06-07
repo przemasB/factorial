bb6e876bdc2d7f5ce0f422bbe551825cd87e8d03
przemasB <s16121@pjwstk.edu.pl>
2023-06-07 20:49:22 +0000

    added some test

--- factorial_test.py
+++ factorial_test.py
@@ -12,6 +12,9 @@ class TestFactorial(unittest.TestCase):
     def test_factorial_3(self):
         self.assertEqual(factorial.factorial(3), 6)
 
+    def test_factorial_1(self):
+        self.assertEqual(factorial.factorial(1), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
